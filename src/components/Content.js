ec9a8fb37cf8d2e88a1ab997853659afc4953b76
Erick Cohen <erickcohen@Ericks-MacBook-Air.local>
2023-01-05 20:30:47 -0500

    updates list content delete and check

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,20 +7,60 @@ const Content = () => {
 		{ id: 0, itemName: 'milk', isChecked: false },
 		{ id: 1, itemName: 'eggs', isChecked: false },
 		{ id: 2, itemName: 'tofu', isChecked: true },
+		{ id: 3, itemName: 'turkey', isChecked: false },
+		{ id: 4, itemName: 'oats', isChecked: true },
 	])
+
+	// function to handle checked list item
+	const handleCheck = (id) => {
+		const listItems = items.map((item) =>
+			item.id === id ? { ...item, isChecked: !item.isChecked } : item
+		)
+		setItems(listItems)
+		// save items to local storage for now
+		localStorage.setItem('groceryList', JSON.stringify(listItems))
+	}
+
+	// function to delete item from list
+	const handleDelete = (id) => {
+		const listItems = items.filter((item) => item.id !== id)
+		setItems(listItems)
+		// save items to local storage for now
+		localStorage.setItem('groceryList', JSON.stringify(listItems))
+	}
+
 	return (
 		<main>
 			<h2>Items</h2>
-			<ul>
-				{items.map((item) => (
-					<li key={item.id}>
-						{item.itemName}
-						<input type='checkbox' checked={item.isChecked} />
-						<label>{item.itemName}</label>
-						<FaRegTrashAlt role='button' tabIndex='0' />
-					</li>
-				))}
-			</ul>
+			{items.length ? (
+				<ul>
+					{items.map((item) => (
+						<li key={item.id}>
+							<input
+								checked={item.isChecked}
+								onChange={() => handleCheck(item.id)}
+								type='checkbox'
+							/>
+							{/* {item.itemName} */}
+							<label
+								onDoubleClick={() => handleCheck(item.id)}
+								style={
+									item.isChecked ? { textDecoration: 'line-through' } : null
+								}
+							>
+								{item.itemName}
+							</label>
+							<FaRegTrashAlt
+								role='button'
+								tabIndex='0'
+								onClick={() => handleDelete(item.id)}
+							/>
+						</li>
+					))}
+				</ul>
+			) : (
+				<h3>Nothing to see here.</h3>
+			)}
 		</main>
 	)
 }
